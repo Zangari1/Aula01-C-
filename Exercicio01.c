7544e328293006b8b5cbecc89256c42176d26c90
Pedro Zangari <145410000+Zangari1@users.noreply.github.com>
2025-08-13 01:31:14 +0000

    Adicionado o ano

---
+++ Exercicio01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main() {
+	printf("Mackenzie-FCI\n");
+	printf("Ciência da Computação\n");
+	printf("Algoritmos e Programação II\n");
+	printf("A FCI foi criada em 1970\n");
+	
+//	system("pause");
+	return 0;
+}
