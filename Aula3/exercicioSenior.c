19be13335a1376cab4d57a8af13095e66d5f5683
Pedro Zangari <145410000+Zangari1@users.noreply.github.com>
2025-08-26 22:50:26 -0300

    Create exercicioSenior.c

---
+++ Aula3/exercicioSenior.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int sortearNumero() {
+    return rand() % 100 + 1;
+}
+
+int lerPalpite() {
+    int palpite;
+    do {
+        printf("Digite seu palpite (1 a 100): ");
+        scanf("%d", &palpite);
+        if (palpite < 1 || palpite > 100) {
+            printf("Valor inválido! Tente novamente.\n");
+        }
+    } while (palpite < 1 || palpite > 100);
+    return palpite;
+}
+
+int verificarPalpite(int palpite, int numeroSorteado) {
+    if (palpite == numeroSorteado) {
+        printf("Parabéns!!! Você acertou!\n");
+        return 1;
+    }
+    else if (palpite < numeroSorteado) {
+        printf("Você chutou muito baixo! Tente novamente.\n");
+    }
+    else {
+        printf("Você chutou muito alto! Tente novamente.\n");
+    }
+    return 0;
+}
+
+int main() {
+    srand(time(NULL));
+
+    int numeroSorteado = sortearNumero();
+    int tentativas = 0;
+    int acertou = 0;
+
+    while (!acertou) {
+        int palpite = lerPalpite();
+        tentativas++;
+        acertou = verificarPalpite(palpite, numeroSorteado);
+    }
+
+    printf("Você acertou em %d tentativas.\n", tentativas);
+
+    return 0;
+}
